f892f8f4d82f15ab216eb748847ffabd61863f60
Ashley King <ashleyot@gmail.com>
2019-04-25 13:54:42 -0400

    initial commit. Got to minute 31:21 on link https://www.gatsbyjs.org/blog/2019-03-21-add-auth0-to-gatsby-livestream/

---
+++ src/pages/utils/auth.js
@@ -0,0 +1,21 @@
+import auth0js from 'auth0-js';
+
+
+dotenv.config()
+
+export const isBrowser = typeof window !== 'undefined';
+
+// Only instantiate Auth0 if we’re in the browser.
+const auth0 = isBrowser
+  ? new auth0js.WebAuth({
+      domain: process.env.AUTH0_DOMAIN,
+      clientID: process.env.AUTH0_CLIENTID,
+      redirectUri: process.env.AUTH0_CALLBACK,
+      responseType: 'token id_token',
+      scope: 'openid profile email'
+    })
+  : {};
+
+  export const login = () => {
+    auth0.authorize()
+  }
