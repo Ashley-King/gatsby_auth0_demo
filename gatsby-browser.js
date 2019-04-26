5bb9f354f51de1cb59fad6ea23ada67d3f4c6b6d
Ashley King <ashleyot@gmail.com>
2019-04-26 09:57:41 -0400

    set and check sessions

---
+++ gatsby-browser.js
@@ -0,0 +1,2 @@
+exports.wrapRootElement
+//49:56 https://www.gatsbyjs.org/blog/2019-03-21-add-auth0-to-gatsby-livestream/
--- src/pages/utils/auth.js
+++ src/pages/utils/auth.js
@@ -1,6 +1,9 @@
 import auth0 from 'auth0-js';
 
-
+const tokens = {
+  idToken: false;
+  accessToken = false;
+}
 
 export const isBrowser = typeof window !== 'undefined';
 
@@ -18,6 +21,33 @@ const auth = isBrowser
   export const login = () => {
     auth.authorize()
   }
+
+
+  export const setSession = ( err, authResult) => {
+    if (err){
+      throw new Error(err);
+    }  
+
+    if (authResult && authResult.accessToken && authResult.idToken){
+      tokens.idToken = authResult.idToken;
+      tokens.accessToken = authResult.accessToken;
+  }
+ 
+  } //setsession
+
+  export const checkSession= () => {
+    auth.checkSession({}, setSession)
+
+  }
+
+
+  export const handlAuthentication = () => {
+    auth.parseHash(setSession); 
+  }
+
+  
+  
+  
   //https://www.gatsbyjs.org/blog/2019-03-21-add-auth0-to-gatsby-livestream/
   // 35:27
   
